a048f63330ff6354dd68549a6e5ae8cbbb74370a
svfabio <svfabio.andres@gmail.com>
2024-09-25 00:34:42 -0400

    Inicio Buscaminas, Clase Tablero con Test sin implementar

---
+++ Buscaminas/TableroTest.java
@@ -0,0 +1,41 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Requerimientos
+
+ */
+
+public class TableroTest
+{
+    /**
+     * Default constructor for test class TableroTest
+     */
+    public TableroTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
